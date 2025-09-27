8ad8fb57426d6935f318502abf410fb6677a22f9
abouabba <you@example.com>
2025-09-27 15:18:46 +0100

    fff

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -1,23 +1,29 @@
 #include "Harl.hpp"
 
-Harl::complain(std::string level)
+void Harl::complain(std::string level)
 {
     std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    void (Harl::*ptrfuncs[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    for (int i = 0; i < 4; i++)
+    {
+        if (level == levels[i])
+        {
+            (this->*ptrfuncs[i])();
+            return;
+        }
+    }
+    std::cout << "UNKNOWN" << std::endl;
+    
     
 }
 
-int main(int argc, char *argv[])
+int main()
 {
-    if (argc != 2)
-    {
-        std::cout << "Error: wrong number of arguments" << std::endl;
-        return 1;
-    }
     Harl harl;
     harl.complain("DEBUG");
     harl.complain("INFO");
     harl.complain("WARNING");
     harl.complain("ERROR");
-    harl.complain("UNKNOWN");
+    harl.complain("BREAKING BAD");
     return 0;
 }
